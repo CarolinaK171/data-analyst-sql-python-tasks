042388376a5ccc812631928ad916d6405520c3c9
CarolinaK171 <kolesnik.ka2010@gmail.com>
2026-08-18 23:09:16 +0200

    Update and rename SQL ДЗ 2.sql to sql_queries_part2.sql

--- sql_queries_part2.sql
+++ sql_queries_part2.sql
@@ -1,21 +1,16 @@
 ﻿1. Посчитайте количество уникальных категорий в таблице Sales
 
-
 select count(distinct category)
 from "Sales"
 
-
 2. Посчитайте сумму всех продаж в разрезе категорий из таблицы Sales
-
 
 select category,
        sum(payed)
 from "Sales"
 group by category
 
-
 3. Для каждого дня найдите максимальную, минимальную и среднюю цену продажи (отсортируйте по возрастанию даты)
-
 
 select sales_date,
        min (payed),
@@ -25,9 +20,7 @@ from "Sales"
 group by sales_date
 order by sales_date
 
-
 4. Для каждого покупателя выведите имя покупателя и сумму и количество покупок (подсказка: количество покупок - количество операций в таблице Sales, вам поможет count(*) ), отсортируйте по имени по убыванию
-
 
 select c.customer_id,
        c.name,
@@ -39,9 +32,7 @@ left join "Sales" as s on c.customer_id = s.customer_id
 group by c.customer_id
 order by c.name
 
-
 5. Найдите количество покупок в разрезе день продажи - категория подсказка (group by можно писать по нескольким полям, в данном случае по двум: категория и день продажи)
-
 
 select sales_date,
        category,
@@ -51,7 +42,6 @@ group by sales_date, category
 order by sales_date
         
 6. Достаньте имена покупателей, которые купили в сумме больше 3 товаров (столбец quantity) и которые зарегистрировались после 1 января 2025 года включительно 
-
 
 select s.customer_id,
        c.name,
@@ -64,11 +54,7 @@ where c.created_at >= '2025-01-01'
 group by s.customer_id, c.name, c.last_name, c.created_at
 having sum(s.quantity) > 3
 
-
-
-
 7. Для каждого дня найдите количество проданных товаров со всех категорий
-
 
 select sales_date,
        sum(quantity)
